cee7a725ff84246720fb947a2626beeaea217624
Colin Anderson <c.anderson1830@gmail.com>
2020-05-28 15:11:05 -0600

    FillService class added

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -26,6 +26,58 @@ public class Person {
         this.personID = personID;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFatherID() {
+        return fatherID;
+    }
+
+    public void setFatherID(String fatherID) {
+        this.fatherID = fatherID;
+    }
+
+    public String getMotherID() {
+        return motherID;
+    }
+
+    public void setMotherID(String motherID) {
+        this.motherID = motherID;
+    }
+
+    public String getSpouseID() {
+        return spouseID;
+    }
+
+    public void setSpouseID(String spouseID) {
+        this.spouseID = spouseID;
+    }
+
+    public String getAssociatedUsername() {
+        return associatedUsername;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
     /**
      * Username to which this person belongs
      */
--- src/Services/FillService.java
+++ src/Services/FillService.java
@@ -1,22 +1,46 @@
 package Services;
 
+import DataAccess.*;
+import Model.AuthToken;
+import Model.Event;
+import Model.Person;
+import Model.User;
 import Result.Response;
 import java.lang.UnsupportedOperationException;
 import java.sql.Connection;
+import java.util.UUID;
 
 /**
  * Responsible for executing fill requests.
  */
 public class FillService extends Service{
-    public FillService() {
+    public FillService() throws Response {
         super();
+        try {
+            userDAO = new UserDAO(db.getConnection());
+            eventDAO = new EventDAO(db.getConnection());
+            personDAO = new PersonDAO(db.getConnection());
+            authTokenDAO = new AuthTokenDAO(db.getConnection());
+        }
+        catch (DataAccessException ex) {
+            throw new Response(ex.getMessage(), false);
+        }
     }
 
     public FillService(Connection conn) {
         super(conn);
+        userDAO = new UserDAO(conn);
+        eventDAO = new EventDAO(conn);
+        personDAO = new PersonDAO(conn);
+        authTokenDAO = new AuthTokenDAO(conn);
     }
 
-
+    UserDAO userDAO;
+    PersonDAO personDAO;
+    EventDAO eventDAO;
+    AuthTokenDAO authTokenDAO;
+    int personCount = 0;
+    int eventCount = 0;
 
     /**
      * Overload of {@code fillDatabase(String username, int generations)} that puts in
@@ -46,7 +70,107 @@ public class FillService extends Service{
      * @exception Response if there was an Internal server error.
      */
     public Response fillDatabase(String username, int generations) throws Response {
-        throw new UnsupportedOperationException("This has not been implemented yet");
+        //Make sure user is registered
+        try {
+            User user = userDAO.find(username);
+            if (user == null) {
+                closeConnection(false);
+                throw new Response("The username does not correspond to a known user", false);
+            }
+            personDAO.clearByUser(username);
+            eventDAO.clearByUser(username);
+            //Recreate the user's Person object
+            Person rootPerson = new Person(user.personID, user.userName, user.firstName, user.lastName,
+                    user.gender, null, null, null);
+            FamilyTreeNode rootNode = new FamilyTreeNode(rootPerson);
+            populateTree(rootNode, 0, generations);
+            fillDatabase(rootNode);
+        }
+        catch (DataAccessException ex) {
+            closeConnection(false);
+            throw new Response(ex.getMessage(), false);
+        }
+        int personCount = (int)Math.pow(2.0, generations);
+        int evenCount = personCount * 3;
+        String msg = String.format("Successfully added %d persons and %d events to the database.",personCount, evenCount);
+        Response rv = new Response(msg, true);
+        closeConnection(true);
+        return rv;
     }
 
+    private void populateTree(FamilyTreeNode childNode, int depth, int maxDepth) {
+        if (!(depth < maxDepth)) {
+            return;
+        }
+        Person child = childNode.thisPerson;
+        Person father = new Person(UUID.randomUUID().toString(), child.getAssociatedUsername(),
+                null, null, "m", null, null, null);
+        Person mother = new Person(UUID.randomUUID().toString(), child.getAssociatedUsername(),
+                null, null,"f", null, null, father.personID);
+        father.setSpouseID(mother.getPersonID());
+        child.setFatherID(father.personID);
+        child.setMotherID(mother.personID);
+        FamilyTreeNode fatherNode = new FamilyTreeNode(father);
+        FamilyTreeNode motherNode = new FamilyTreeNode(mother);
+        childNode.setFather(fatherNode);
+        populateTree(fatherNode, depth + 1, maxDepth);
+        childNode.setMother(motherNode);
+        populateTree(motherNode, depth + 1, maxDepth);
+    }
+
+    private void fillDatabase(FamilyTreeNode childNode) throws DataAccessException {
+        if (childNode == null) {
+            return;
+        }
+    //Insert Person
+        personDAO.insert(childNode.thisPerson);
+    //Insert Events
+        for (Event event : childNode.getEvents()) {
+            eventDAO.insert(event);
+        }
+    //Insert Parents
+        fillDatabase(childNode.getFather());
+        fillDatabase(childNode.getMother());
+    }
+
+    private class FamilyTreeNode {
+        public FamilyTreeNode(Person person) {
+            thisPerson = person;
+            events = new Event[3];
+            events[0] = new Event(UUID.randomUUID().toString(), person.getAssociatedUsername(), person.personID,
+                    0,0,null,null,"Birth",0);
+            events[1] = new Event(UUID.randomUUID().toString(), person.getAssociatedUsername(), person.personID,
+                    0,0,null,null,"Death",0);
+            events[2] = new Event(UUID.randomUUID().toString(), person.getAssociatedUsername(), person.personID,
+                    0,0,null,null,"Marriage", 0);
+
+        }
+        Person thisPerson;
+        FamilyTreeNode father;
+        FamilyTreeNode mother;
+        Event[] events;
+        public FamilyTreeNode getFather() {
+            return father;
+        }
+
+        public void setFather(FamilyTreeNode father) {
+            this.father = father;
+        }
+
+        public FamilyTreeNode getMother() {
+            return mother;
+        }
+
+        public void setMother(FamilyTreeNode mother) {
+            this.mother = mother;
+        }
+
+        public Person getThisPerson() {
+            return thisPerson;
+        }
+
+        public Event[] getEvents() {
+            return events;
+        }
+    }
 }
